0374fc3d3257c28a929d5feabde146b8243c214b
ghaziirfan123 <70496821+ghaziirfan123@users.noreply.github.com>
2020-11-01 23:55:00 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-# My name is Mohammad Ghazi Irfan and my ID ias 1626488
+# My name is Mohammad Ghazi Irfan and my ID is 1626488
 
-# empty dictionary
-dic={}
-#loop to accept 5 player's details
-for i in range(5):
-    #accepting jersey number
-    num=int(input("Enter player's jersey number: "))
-    #accepting rating
-    rating=int(input("Enter player's rating: "))
-    #adding the details to dictionary
-    dic[num]=rating
-#sorting the key's in dictionary
-for i in sorted(dic):
-    #printing key and value
-    print(str(i)+" : "+str(dic[i]))
+class Team:
+    def __init__(self):
+        self.team_name = 'none'
+        self.team_wins = 0
+        self.team_losses = 0
 
-# the code gives the answer but i cannot figure out how to get the credit in zybooks
+    def set_team_name(self, team_name):
+        self.team_name = team_name
+
+    def set_team_wins(self, team_wins):
+        self.team_wins = team_wins
+
+    def set_team_losses(self, team_losses):
+        self.team_losses = team_losses
+
+    # TODO: Define get_win_percentage()
+    def get_win_percentage(self):
+        percent = self.team_wins / (self.team_wins + self.team_losses)
+        return percent
+
+
+if __name__ == "__main__":
+
+    team = Team()
+
+    team_name = input()
+    team_wins = int(input())
+    team_losses = int(input())
+
+    team.set_team_name(team_name)
+    team.set_team_wins(team_wins)
+    team.set_team_losses(team_losses)
+
+    if team.get_win_percentage() >= 0.5:
+        print('Congratulations, Team', team.team_name, 'has a winning average!')
+    else:
+        print('Team', team.team_name, 'has a losing average.')
